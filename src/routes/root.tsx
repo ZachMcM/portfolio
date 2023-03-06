7873554cc0bf703f791d3d70741ad3a02dbbad1d
Zach McMullen <zachmcmullen04@gmail.com>
2023-03-05 18:05:30 -0800

    home

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import Home from './Home'
+import Home from './home'
 import Projects from './projects'
 import '../App.css'
 
